efdac2d9849b7fa48396a0dfb5b22a10982027df
zarigata <vallestrauch@yahoo.com>
2022-11-26 01:52:50 -0300

    WITH AWSER AND MUTCH MORE

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 config = loads(Path("config.json").read_text())
 
 #mandatory shit.... new API... OLD CUM
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 # CONFIOG FILE SO THAT DOES NOT SENT TO GITHUB
 token = config["token"]
@@ -29,7 +29,11 @@ async def on_message(msg):
     if msg.content == "hello":
         await msg.channel.send("FUCK OFF " + username)
 
-@bot.event
+@bot.event #sends a greeting cart for the player NOT WORKING
 async def on_join(member):
-
+    guild = member.guild
+    guildname = guild.name
+    dmchannel = await member.create_dm()
+    await dmchannel.send(f"GREETINGS FROM {guildname}")
+    print("NEW USER")
 bot.run(token)
