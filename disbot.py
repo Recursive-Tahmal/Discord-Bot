1e1869e88528d56456b91635c0452c4d33841fd4
zarigata <vallestrauch@yahoo.com>
2022-11-30 00:49:22 -0300

    HIKARY LIGHTBULB. WHATS NEXT? HIKARY L.E.D?

--- disbot.py
+++ disbot.py
@@ -10,6 +10,7 @@ import lightbulb
 config = loads(Path("config.json").read_text())
 bot = lightbulb.BotApp(
     token=config["token"],
+    default_enabled_guilds=(1045906948341633115)
 )
 
 
@@ -24,6 +25,13 @@ async def bot_start(event):
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping (ctx):
     await ctx.respond('XING XONG')
+
+@bot.command
+@lightbulb.command('help', 'Inform you what are the commands of the bot')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def help (ctx):
+    await ctx.respond('PING = Shows you pong')
+
 
 
 bot.run()
