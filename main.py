f4c3eeb96288d7bb2c479f7e50f763518f048599
Tahmal <tahmal565@gmail.com>
2022-12-11 10:24:59 +0300

    Added calculate how much time left for the given time

--- main.py
+++ main.py
@@ -1,48 +1,97 @@
-import discord  # library that calls discord
+import hikari
+import lightbulb
+import requests
+import datetime
 from json import loads
 from pathlib import Path
-#===========================================================#
 
+# python -m venv env
+
+# orders matter for dicorators
+
+# bot token
 config = loads(Path("config.json").read_text())
-
-#mandatory shit.... new API... OLD CUM
-intents = discord.Intents.all()
-intents.message_content = True
-# CONFIOG FILE SO THAT DOES NOT SENT TO GITHUB
-token = config["token"]
+bot = lightbulb.BotApp(
+    token=config["token"],
+)
 
 
+# @bot.listen(hikari.GuildMessageCreateEvent)  # listens and prints on the console what's being typed
+# async def print_message(event):
+#     print(event.content)
 
 
-
-#LOGS THAT BOT IS OK
-bot = discord.Client(intents=intents)
-@bot.event
-async def on_ready():
-    print("BOT IS OPERATIONAL")
-
-@bot.event #replies to the message with a verry calm awnser
-async def on_message(msg):
-    username = msg.author.display_name
-    if msg.author == bot.user:
-        return
-    if msg.content == "hello":
-        await msg.channel.send("FUCK OFF " + username)
-
-@bot.event #sends a greeting cart for the player NOT WORKING
-async def on_join(member):
-    guild = member.guild
-    guildname = guild.name
-    dmchannel = await member.create_dm()
-    await dmchannel.send(f"GREETINGS FROM {guildname}")
-    print("NEW USER")
+@bot.listen(hikari.StartedEvent)  # console types bot has started
+async def bot_started(event):
+    print("Bot has started!")
 
 
+@bot.command
+@lightbulb.command("ping", "Says pong!")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):  # say ping and respond to pong
+    await ctx.respond("Pong!")
 
 
-#====================#
-#        RUN         #
-#====================#
-bot.run(token)
+# grouping commands
+@bot.command
+@lightbulb.command("group", "This is a group")
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def my_group(ctx):
+    pass
 
-#test123Hello
+
+@my_group.child
+@lightbulb.command("subcommand", "This is a subcommand")
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def subcommand(ctx):
+    await ctx.respond("I am a subcommand!")
+
+
+# this will add 2 numbers
+@bot.command
+@lightbulb.option("num2", "The second number", type=float)  # options
+@lightbulb.option("num1", "The first number", type=float)
+@lightbulb.command("add", "Add two numbers together")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def addnums(ctx):
+    await ctx.respond(ctx.options.num2 + ctx.options.num1)
+
+
+# API test
+@bot.command
+@lightbulb.command("joke", "tells a joke")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def jokes(ctx):
+    response = requests.get("https://api.chucknorris.io/jokes/random")
+    fox = response.json()
+    await ctx.respond(fox["value"])
+
+
+@bot.command
+@lightbulb.command("cat", "sends a cat picture")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def cat(ctx):
+    response = requests.get("https://aws.random.cat/meow")
+    cat = response.json()["file"]
+    await ctx.respond(cat)
+
+
+# calculates how much left for the given time
+@bot.command
+@lightbulb.option("year", "Enter the year", type=int)
+@lightbulb.option("month", "Enter the month", type=int)
+@lightbulb.option("day", "Enter the day", type=int)
+@lightbulb.command("date", "tells how long till the given date")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def date(ctx):
+    present = datetime.datetime.now()
+    given_date = datetime.datetime(ctx.options.year, ctx.options.month, ctx.options.day, 0, 0, 0)
+    await ctx.respond(given_date - present)
+
+
+# TODO Make a command that tells people what's trending upon your friends -Example Minecraft server ip 192.168.1.1
+
+
+#---------------------------MAIN------------------------------------#
+bot.run()
